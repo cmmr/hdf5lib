2649d7473ddd464dcac90268c05682341e70f4f2
dansmith01 <dansmith@orst.edu>
2025-11-23 10:48:55 -0800

    H5lib_settings

---
+++ src/H5lib_settings.c
@@ -0,0 +1,12 @@
+#include "H5private.h"
+
+const char H5libhdf5_settings[] = 
+    "HDF5 Library Version: 2.0.0\n"
+    "   configured on: R-Package-Build\n"
+    "   configured by: R-User\n"
+    "      build mode: C-Only\n"
+    "support: none";
+
+const char H5build_settings[] = 
+    "HDF5 Library Version: 2.0.0\n"
+    "   configured on: R-Package-Build\n";
