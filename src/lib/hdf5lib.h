d3adaec478f60621ae18d46c6c507639ce780514
Daniel Smith <dansmith@orst.edu>
2026-03-13 02:35:32 +0100

    `extern "C"`

--- src/lib/hdf5lib.h
+++ src/lib/hdf5lib.h
@@ -2,8 +2,16 @@
 #define HDF5LIB_FILTERS_H
 #include <hdf5.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Expose the functions to downstream packages */
 herr_t hdf5lib_register_all_filters(void);
 herr_t hdf5lib_destroy_all_filters(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
